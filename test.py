e775d39838cf493429cf8ca8d1d73ac8bc50a0c2
Ginger <75683114+gingershaped@users.noreply.github.com>
2025-03-05 11:51:36 -0500

    Add caching by parameters

--- test.py
+++ test.py
@@ -5,60 +5,93 @@ from returncache import returncache
 
 now = lambda: datetime.fromtimestamp(1)
 
+
 class TestReturncache(unittest.TestCase):
 
     def test_sync_cached(self):
         invocations = 0
 
-        @returncache(now)
-        def foo():
+        @returncache(now=now)
+        def foo(value):
             nonlocal invocations
             invocations += 1
-            return (datetime.fromtimestamp(2), ":3")
+            return (datetime.fromtimestamp(2), value)
 
-        self.assertEqual(foo(), ":3")
-        self.assertEqual(foo(), ":3")
+        self.assertEqual(foo(":3"), ":3")
+        self.assertEqual(foo(":3"), ":3")
+        self.assertEqual(foo(":3c"), ":3c")
+        self.assertEqual(invocations, 2)
+
+    def test_sync_no_parameter_keying(self):
+        invocations = 0
+
+        @returncache(keyed_by_parameters=False, now=now)
+        def foo(value):
+            nonlocal invocations
+            invocations += 1
+            return (datetime.fromtimestamp(2), value)
+
+        self.assertEqual(foo(":3"), ":3")
+        self.assertEqual(foo(":3c"), ":3")
         self.assertEqual(invocations, 1)
 
     def test_sync_cache_miss(self):
         invocations = 0
 
-        @returncache(now)
-        def foo():
+        @returncache(now=now)
+        def foo(value):
             nonlocal invocations
             invocations += 1
-            return (datetime.fromtimestamp(0), ":3")
+            return (datetime.fromtimestamp(0), value)
 
-        self.assertEqual(foo(), ":3")
-        self.assertEqual(foo(), ":3")
-        self.assertEqual(invocations, 2)
+        self.assertEqual(foo(":3"), ":3")
+        self.assertEqual(foo(":3"), ":3")
+        self.assertEqual(foo(":3c"), ":3c")
+        self.assertEqual(invocations, 3)
+
 
 class TestAsyncReturncache(unittest.IsolatedAsyncioTestCase):
     async def test_async_cached(self):
         invocations = 0
 
-        @returncache(now)
-        async def foo():
+        @returncache(now=now)
+        async def foo(value):
             nonlocal invocations
             invocations += 1
-            return (datetime.fromtimestamp(2), ":3")
+            return (datetime.fromtimestamp(2), value)
 
-        self.assertEqual(await foo(), ":3")
-        self.assertEqual(await foo(), ":3")
+        self.assertEqual(await foo(":3"), ":3")
+        self.assertEqual(await foo(":3"), ":3")
+        self.assertEqual(await foo(":3c"), ":3c")
+        self.assertEqual(invocations, 2)
+
+    async def test_async_no_parameter_keying(self):
+        invocations = 0
+
+        @returncache(keyed_by_parameters=False, now=now)
+        async def foo(value):
+            nonlocal invocations
+            invocations += 1
+            return (datetime.fromtimestamp(2), value)
+
+        self.assertEqual(await foo(":3"), ":3")
+        self.assertEqual(await foo(":3c"), ":3")
         self.assertEqual(invocations, 1)
 
     async def test_async_cache_miss(self):
         invocations = 0
 
-        @returncache(now)
-        async def foo():
+        @returncache(now=now)
+        async def foo(value):
             nonlocal invocations
             invocations += 1
-            return (datetime.fromtimestamp(0), ":3")
+            return (datetime.fromtimestamp(0), value)
 
-        self.assertEqual(await foo(), ":3")
-        self.assertEqual(await foo(), ":3")
-        self.assertEqual(invocations, 2)
+        self.assertEqual(await foo(":3"), ":3")
+        self.assertEqual(await foo(":3"), ":3")
+        self.assertEqual(await foo(":3c"), ":3c")
+        self.assertEqual(invocations, 3)
+
 
 if __name__ == "__main__":
     unittest.main()
